a9810fd8b97a9f99033b14beed13660dc53c28ed
陈明超 <mingchao.chen@mihoyo.com>
2021-01-05 16:30:13 +0800

    add test

--- test2.py
+++ test2.py
@@ -1,9 +1,12 @@
 import os
 import logging
+import time
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(name)s %(levelname)s %(message)s",
                     datefmt='%m-%d %H:%M:%S')
 logger = logging.getLogger("test")
 
+
 print(os.path.abspath('/mnt/nfs-storage'))
 logger.info('test')
+time.sleep(100)
