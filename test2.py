113838ca1fffcfb4c85ae8b2ce017ae879cef283
陈明超 <mingchao.chen@mihoyo.com>
2021-01-05 16:26:32 +0800

    add test

--- test2.py
+++ test2.py
@@ -1,3 +1,9 @@
 import os
+import logging
 
-print(os.path.abspath('./'))
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(name)s %(levelname)s %(message)s",
+                    datefmt='%m-%d %H:%M:%S')
+logger = logging.getLogger("test")
+
+print(os.path.abspath('/mnt/nfs-storage'))
+logger.info('test')
